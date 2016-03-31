5f8fd2f8b63322220115204c10317dc64aa57d22
Benny <bennycreative@gmail.com>
2016-03-30 18:33:44 -0700

    clean up Unify `App`

--- public/assets/js/one.app.js
+++ public/assets/js/one.app.js
@@ -6,152 +6,11 @@
  * Website: http://htmlstream.com
 */
 
-var App = function() {
+var App;
 
-  function handleBootstrap() {
-    /*Bootstrap Carousel*/
-    jQuery('.carousel').carousel({
-      interval: 15000,
-      pause: 'hover'
-    });
+(function() {
 
-    /*Tooltips*/
-    jQuery('.tooltips').tooltip();
-    jQuery('.tooltips-show').tooltip('show');
-    jQuery('.tooltips-hide').tooltip('hide');
-    jQuery('.tooltips-toggle').tooltip('toggle');
-    jQuery('.tooltips-destroy').tooltip('destroy');
-
-    /*Popovers*/
-    jQuery('.popovers').popover();
-    jQuery('.popovers-show').popover('show');
-    jQuery('.popovers-hide').popover('hide');
-    jQuery('.popovers-toggle').popover('toggle');
-    jQuery('.popovers-destroy').popover('destroy');
-  }
-
-  var handleFullscreen = function() {
-    var WindowHeight = $(window).height();
-
-    if ($(document.body).hasClass("promo-padding-top")) {
-      HeaderHeight = $(".header").height();
-    } else {
-      HeaderHeight = 0;
-    }
-
-    $(".fullheight").css("height", WindowHeight - HeaderHeight);
-
-    $(window).resize(function() {
-      var WindowHeight = $(window).height();
-      $(".fullheight").css("height", WindowHeight - HeaderHeight);
-    });
-  }
-
-  // handleLangs
-  function handleLangs() {
-    $(".lang-block").click(function() {
-      console.log("click!");
-    });
-  }
-
-  var handleValignMiddle = function() {
-    $(".valign__middle").each(function() {
-      $(this).css("padding-top", $(this).parent().height() / 2 - $(this).height() / 2);
-    });
-    $(window).resize(function() {
-      $(".valign__middle").each(function() {
-        $(this).css("padding-top", $(this).parent().height() / 2 - $(this).height() / 2);
-      });
-    });
-  }
-
-  function handleHeader() {
-    var offsetHeight;
-    if(window.location.pathname.match('index.html')) {
-      $('#brave-logo').attr('src', 'assets/img/brave_logo_horz_reversed.svg');
-      $('.navbar-nav.brave-nav').addClass('home');
-    }
-    else {
-      $('#brave-logo').attr('src', 'assets/img/brave_logo_horz.svg');
-    }
-    $(window).scroll(function() {
-      if ($(".navbar").offset().top > 150) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-        $('#brave-logo').attr('src', 'assets/img/brave_logo_horz.svg');
-      } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        if(window.location.pathname.match('index.html')) {
-          $('#brave-logo').attr('src', 'assets/img/brave_logo_horz_reversed.svg');
-        }
-      }
-    });
-    offsetHeight = 116;
-    $('body').scrollspy({ offset: offsetHeight + 1 });
-    $(function() {
-      $('.page-scroll a, .scroll-button').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top - offsetHeight
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-      });
-    });
-    $(window).scroll(function() {
-      $(".navbar-collapse.in").collapse('hide');
-    });
-  }
-
-  function isPlatform(userAgent) {
-    return !!window.navigator.userAgent.match && window.navigator.userAgent.match(userAgent);
-  }
-
-  function listenToDownloadButton(url) {
-    $('#brave-download').click(function(event) {
-      return window.location.href = url;
-    });
-  }
-
-  function listenToVideoButton() {
-    return $('#brave-video, #brave-overlay').click(handleVideoButton);
-  }
-
-  function toggleVideoButton() {
-    if($('#brave-overlay').hasClass('show')) {
-      $('body').removeClass('no-scroll');
-      $('#brave-overlay').removeClass('show');
-      setTimeout(function() {
-        $('#brave-overlay').css('display', 'none');
-      }, 500);
-      return;
-    }
-    $('#brave-overlay').css('display', 'block');
-    setTimeout(function() {
-      $('body').addClass('no-scroll');
-      $('#brave-overlay').addClass('show');
-    }, 100);
-  }
-
-  function configureDownloadButton(platform, index) {
-    if(isPlatform(platform.userAgent)) {
-      $('.control-group').find('.label').not('a').html('For ' + platform.name + ' or later.');
-      listenToDownloadButton(platform.url);
-    }
-  }
-
-  function handleUserAgent(platforms) {
-    var buttons = $('.brave-hero').find('.btn').remove();
-    listenToDownloadButton('https://github.com/brave/browser-laptop/releases');
-    platforms.forEach(configureDownloadButton, buttons);
-  }
-
-  function handleVideoButton(event) {
-    if((event.target.id !== 'brave-overlay') && (event.target.id !== 'brave-video')) {
-      return false;
-    }
-    return toggleVideoButton();
-  }
-
-  return {
+  App = {
 
     platforms: [
       { name: 'Linux x64, Ubuntu', userAgent: 'Linux|Ubuntu', url: 'https://laptop-updates.brave.com/latest/linux64' },
@@ -161,14 +20,65 @@ var App = function() {
       { name: 'Android 4.1', userAgent: 'Android', url: 'https://play.google.com/store/apps/details?id=com.linkbubble.playstore' }
     ],
 
-    init: function() {
-      handleUserAgent(this.platforms);
-      handleHeader();
-      handleBootstrap();
-      //handleLangs();
-      handleFullscreen();
-      handleValignMiddle();
-      listenToVideoButton();
+    toggleVideoButton: function() {
+      if($('#brave-overlay').hasClass('show')) {
+        $('body').removeClass('no-scroll');
+        $('#brave-overlay').removeClass('show');
+        setTimeout(function() {
+          $('#brave-overlay').css('display', 'none');
+        }, 500);
+        return;
+      }
+      $('#brave-overlay').css('display', 'block');
+      setTimeout(function() {
+        $('body').addClass('no-scroll');
+        $('#brave-overlay').addClass('show');
+      }, 100);
+    },
+
+    handleValignMiddle: function() {
+      $('.valign__middle').each(function() {
+        $(this).css('padding-top', $(this).parent().height() / 2 - $(this).height() / 2);
+      });
+      $(window).resize(function() {
+        $('.valign__middle').each(function() {
+          $(this).css('padding-top', $(this).parent().height() / 2 - $(this).height() / 2);
+        });
+      });
+    },
+
+    handleVideoButton: function(event) {
+      if((event.target.id !== 'brave-overlay') && (event.target.id !== 'brave-video')) {
+        return false;
+      }
+      return this.toggleVideoButton();
+    },
+
+    listenToDownloadButton: function(url) {
+      $('#brave-download').click(function(event) {
+        return window.location.href = url;
+      });
+    },
+
+    listenToVideoButton: function() {
+      return $('#brave-video, #brave-overlay').click(this.handleVideoButton.bind(this));
+    },
+
+    configureDownloadButton: function(platform, index) {
+      if(this.isPlatform(platform.userAgent)) {
+        $('.control-group').find('.label').not('a').html('For ' + platform.name + ' or later.');
+        this.listenToDownloadButton(platform.url);
+      }
+    },
+
+    reactToUserAgent: function(platforms) {
+      var buttons = $('.brave-hero').find('.btn').remove();
+      this.listenToDownloadButton('https://github.com/brave/browser-laptop/releases');
+      platforms.forEach(this.configureDownloadButton.bind(this), buttons);
+    },
+
+    isPlatform: function(userAgent) {
+      return (window.navigator.userAgent.match && window.navigator.userAgent.match(userAgent));
     },
 
     initCounter: function() {
@@ -180,17 +90,79 @@ var App = function() {
 
     initParallaxBg: function() {
       $(window).load(function() {
-        jQuery('.parallaxBg').parallax("50%", 0.4);
-        jQuery('.parallaxBg1').parallax("50%", 0.2);
+        jQuery('.parallaxBg').parallax('50%', 0.4);
+        jQuery('.parallaxBg1').parallax('50%', 0.2);
       });
     },
 
     initParallaxBg2: function() {
       $(window).load(function() {
-        jQuery('.parallaxBg').parallax("50%", "50%");
+        jQuery('.parallaxBg').parallax('50%', '50%');
       });
     },
 
+    initHeader: function() {
+      var offsetHeight;
+      if(window.location.pathname.match('index.html')) {
+        $('#brave-logo').attr('src', 'assets/img/brave_logo_horz_reversed.svg');
+        $('.navbar-nav.brave-nav').addClass('home');
+      }
+      else {
+        $('#brave-logo').attr('src', 'assets/img/brave_logo_horz.svg');
+      }
+      $(window).scroll(function() {
+        if ($('.navbar').offset().top > 150) {
+          $('.navbar-fixed-top').addClass('top-nav-collapse');
+          $('#brave-logo').attr('src', 'assets/img/brave_logo_horz.svg');
+        } else {
+          $('.navbar-fixed-top').removeClass('top-nav-collapse');
+          if(window.location.pathname.match('index.html')) {
+            $('#brave-logo').attr('src', 'assets/img/brave_logo_horz_reversed.svg');
+          }
+        }
+      });
+      offsetHeight = 116;
+      $('body').scrollspy({ offset: offsetHeight + 1 });
+      $(function() {
+        $('.page-scroll a, .scroll-button').bind('click', function(event) {
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - offsetHeight
+          }, 1500, 'easeInOutExpo');
+          event.preventDefault();
+        });
+      });
+      $(window).scroll(function() {
+        $('.navbar-collapse.in').collapse('hide');
+      });
+    },
+
+    initBootstrapUI: function() {
+      jQuery('.carousel').carousel({
+        interval: 15000,
+        pause: 'hover'
+      });
+      jQuery('.tooltips').tooltip();
+      jQuery('.tooltips-show').tooltip('show');
+      jQuery('.tooltips-hide').tooltip('hide');
+      jQuery('.tooltips-toggle').tooltip('toggle');
+      jQuery('.tooltips-destroy').tooltip('destroy');
+      jQuery('.popovers').popover();
+      jQuery('.popovers-show').popover('show');
+      jQuery('.popovers-hide').popover('hide');
+      jQuery('.popovers-toggle').popover('toggle');
+      jQuery('.popovers-destroy').popover('destroy');
+    },
+
+    init: function(params) {
+      this.initHeader();
+      this.initBootstrapUI();
+      this.reactToUserAgent(this.platforms);
+      this.handleValignMiddle();
+      this.listenToVideoButton();
+      return this;
+    }
+
   };
 
-}();
+}());
