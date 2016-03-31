278c4081f9ca531c470fde4158c5f6c50125c40c
bradleyrichter <brad@brave.com>
2016-03-31 01:05:41 -0700

    test two

--- server.js
+++ server.js
@@ -102,6 +102,15 @@ server.register(require('inert'), (err) => {
     })
   })
 
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+  
   // Serves static files out of public/
   server.route({
     method: 'GET',
