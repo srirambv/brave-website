330006cd429d3cdf57ed9fad0ea83102fa849782
bradleyrichter <brad@brave.com>
2016-03-31 01:33:31 -0700

    deleted character

--- public/assets/js/one.app.js
+++ public/assets/js/one.app.js
@@ -128,7 +128,7 @@ var App;
     },
 
     initHeader: function() {
-      if(window.location.pathname.match('/')) {
+      if(window.location.pathname.match('/') {
         $('#brave-logo').attr('src', 'assets/img/brave_logo_horz_reversed.svg');
         $('.navbar-nav.brave-nav, .navbar-toggle').addClass('home');
       }
