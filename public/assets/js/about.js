faacd900ea247c2d36aa5de6034dd93965ded49b
Benny <bennycreative@gmail.com>
2016-04-05 16:05:43 -0700

    add about page component, add missing closing tag

---
+++ public/assets/js/about.js
@@ -0,0 +1,26 @@
+/*
+  Brave Front-end Framework
+  https://github.com/brave/brave-website
+*/
+
+var Brave = Brave || window.Brave || { app: {} };
+
+/*
+  About View
+*/
+
+(function() {
+
+  Brave.app.about = new Brave.View({
+
+    name: 'about',
+
+    events: [],
+
+    init: function() {
+
+    }
+
+  });
+
+}());
