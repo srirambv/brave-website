6530dbb82cad344902b9f32f3403e2fc9ae51d9e
bradleyrichter <brad@brave.com>
2016-04-16 10:22:18 -0700

    edits and asserts for press assets section on about page

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -31,7 +31,7 @@ var Brave = Brave || window.Brave || { app: {} };
       hasPhotographicHeader: true,
 
       platforms: [
-        { name: 'Linux x64, Ubuntu', userAgent: 'Linux|Ubuntu', url: 'https://laptop-updates.brave.com/latest/linux64' },
+        { name: 'Linux x64, Ubuntu', userAgent: 'Linux|Ubuntu', url: 'https://github.com/brave/browser-laptop/blob/master/docs/linuxInstall.md' },
         { name: 'Mac OS 10.9', userAgent: 'Macintosh', url: 'https://laptop-updates.brave.com/latest/osx' },
         { name: 'Windows 7 x64', userAgent: 'Windows', url: 'https://laptop-updates.brave.com/latest/winx64' },
         { name: 'iOS 8', userAgent: 'iPhone|iPod|iPad', url: 'https://itunes.apple.com/us/app/brave-web-browser/id1052879175' },
